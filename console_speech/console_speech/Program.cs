f3a56b783bcd8afdbd2b1327967bf75dc8ceb899
David <dop4398@rit.edu>
2019-03-19 23:42:09 -0400

    Comments!

--- console_speech/console_speech/Program.cs
+++ console_speech/console_speech/Program.cs
@@ -41,31 +41,32 @@ namespace console_speech
                 gb_StartStop.Append(ch_StartStopCommands);
                 Grammar g_StartStop = new Grammar(gb_StartStop);
 
-                // Make a Choices collection, add numbers (strings) to it.
-                Choices ch_Numbers = new Choices();
-                ch_Numbers.Add("1");
-                ch_Numbers.Add("2");
-                ch_Numbers.Add("3");
-                ch_Numbers.Add("4");
+                // Make a Choices collection, add numbers (array of strings) to it.
+                string[] numbers = new string[] { "1", "2", "3", "4" };
+                Choices ch_Numbers = new Choices(numbers);
 
                 // List of specific examples of what we're listening for.
                 //  Then determine a corresponding general template.
                 //  The template is a GrammarBuilder, and the specific values that go into it are Choices.
+                // These are all of the words that this Grammar object will be listening for.
                 GrammarBuilder gb_WhatIsXplusY = new GrammarBuilder();
                 gb_WhatIsXplusY.Append("What is");
                 gb_WhatIsXplusY.Append(ch_Numbers);
                 gb_WhatIsXplusY.Append("plus");
                 gb_WhatIsXplusY.Append(ch_Numbers);
-
                 Grammar g_WhatIsXplusY = new Grammar(gb_WhatIsXplusY);
 
+                // Here we have a lot of flexibility when defining grammars.
+                // We could create new Grammar objects for other commands.
+
+                // Pass them into the speech recognizer once they're all created.
                 sre.LoadGrammarAsync(g_StartStop);
                 sre.LoadGrammarAsync(g_WhatIsXplusY);
-                sre.RecognizeAsync(RecognizeMode.Multiple);
+                sre.RecognizeAsync(RecognizeMode.Multiple); // Multiple for when we have more than one Grammar.
 
                 while(done == false)
                 {
-                    ;
+                    ; // This allows the console app shell to stay alive.
                 }
 
                 Console.ReadLine();
@@ -77,12 +78,16 @@ namespace console_speech
             }
         } // Main
 
+        // The recognized text is stored in the SpeechRecognizedEventArgs Result here.
         static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             string txt = e.Result.Text;
+            // The confidence value (between 0.0 and 1.0) will tend to fall 
+            //  as more commands are being listened for.
             float confidence = e.Result.Confidence;
             Console.WriteLine("\nRecognized: " + txt);
 
+            // If the confidence is too low, don't do anything.
             if(confidence < 0.60)
             {
                 return;
@@ -101,7 +106,7 @@ namespace console_speech
             {
                 return;
             }
-
+            // End the program upon hearing "farewell"
             if(txt.IndexOf("farewell") >= 0)
             {
                 ((SpeechRecognitionEngine)sender).RecognizeAsyncCancel();
@@ -109,8 +114,8 @@ namespace console_speech
                 Console.WriteLine("(Speaking: Farewell)");
                 ss.Speak("Farewell");
             }
-
-            if(txt.IndexOf("what") >= 0 && txt.IndexOf("plus") >= 0)
+            // When this specific question is asked, return the sum of the two given numbers.
+            if(txt.IndexOf("What") >= 0 && txt.IndexOf("plus") >= 0)
             {
                 string[] words = txt.Split(' ');
                 int num1 = int.Parse(words[2]);
