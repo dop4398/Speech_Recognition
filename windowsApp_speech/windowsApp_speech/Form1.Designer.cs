c001d8af23c9826f38018b611b43cd2a7f40b1ef
David <dop4398@rit.edu>
2019-03-29 17:34:51 -0400

    The test windows app is finished, got it to recognize voice commands and have a browser navigate to specific links when said aloud.

---
+++ windowsApp_speech/windowsApp_speech/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace windowsApp_speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 111);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(295, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(325, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Listening";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(38, 242);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(778, 455);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 709);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ windowsApp_speech/windowsApp_speech/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System.Globalization;
+
+namespace windowsApp_speech
+{
+    public partial class Form1 : Form
+    {
+        static CultureInfo ci = new CultureInfo("en-us");
+        static SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+
+        public Form1()
+        {
+            InitializeComponent();
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += sre_SpeechRecognized;
+            Grammar g_HellowGoodbye = GetHelloGoodbyeGrammar();
+            Grammar g_SetTextBox = GetTextBox1TextGrammar();
+            Grammar g_WebBrowser = GetWebBrowserGrammer();
+            sre.LoadGrammarAsync(g_HellowGoodbye);
+            sre.LoadGrammarAsync(g_SetTextBox);
+            sre.LoadGrammarAsync(g_WebBrowser);
+        }
+
+        // Helper method
+        static Grammar GetHelloGoodbyeGrammar()
+        {
+            // Makes choices
+            Choices ch_HelloGoodbye = new Choices();
+            ch_HelloGoodbye.Add("hello");
+            ch_HelloGoodbye.Add("goodbye");
+
+            // Make a Grammar Builder
+            GrammarBuilder gb_result = new GrammarBuilder(ch_HelloGoodbye);
+
+            // Make a Grammar out of that, then return it.
+            Grammar g_result = new Grammar(gb_result);
+            return g_result;
+        }
+
+        // Helper method
+        // Recognizes "set text box 1 ..."
+        static Grammar GetTextBox1TextGrammar()
+        {
+            Choices ch_Colors = new Choices();
+            ch_Colors.Add(new string[] { "red", "white", "blue" });
+
+            GrammarBuilder gb_result = new GrammarBuilder();
+            gb_result.Append("set text box 1");
+            gb_result.Append(ch_Colors);
+
+            Grammar g_result = new Grammar(gb_result);
+            return g_result;
+        }
+
+        // Helper method
+        //  Changes the browser window
+        static Grammar GetWebBrowserGrammer()
+        {
+            Choices ch_Songs = new Choices();
+            ch_Songs.Add(new string[]
+            {
+                "all star by smash mouth",
+                "everybodies circulation",
+                "megalovania"
+            });
+
+            GrammarBuilder gb_result = new GrammarBuilder();
+            gb_result.Append("alexa play");
+            gb_result.Append(ch_Songs);
+
+            Grammar g_result = new Grammar(gb_result);
+            return g_result;
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string txt = e.Result.Text;
+            float conf = e.Result.Confidence;
+
+            if(conf < 0.65)
+            {
+                return;
+            }
+
+            // Change the text of listBox1 and textBox1
+            // A method invoker is needed because the speech recognizer is running
+            //  in a different thread from the Windows Forms UI.
+            this.Invoke(new MethodInvoker(() =>
+            {
+                // WinForm specific
+                listBox1.Items.Add("I heard you say: " + txt);
+            }));
+
+            if (txt.IndexOf("text") >= 0 && txt.IndexOf("box") >= 0
+                && txt.IndexOf("1") >= 0)
+            {
+                string[] words = txt.Split(' ');
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    // WinForm specific
+                    textBox1.Text = words[4];
+                }));
+            }
+
+            // Pull up the youtube videos of the subsequent commands in the web browser
+            if(txt.IndexOf("alexa") >= 0 && txt.IndexOf("play") >= 0)
+            {
+                string url = "";
+
+                if(txt.IndexOf("all star") >= 0)
+                {
+                    url = "https://youtu.be/L_jWHffIx5E?t=36";
+                }           
+                else if(txt.IndexOf("everybodies curculation") >= 0)
+                {
+                    url = "https://www.youtube.com/watch?v=RQmEERvqq70";
+                }
+                else if(txt.IndexOf("megalovania") >= 0)
+                {
+                    url = "https://www.youtube.com/watch?v=ZcoqR9Bwx1Y";
+                }
+
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    webBrowser1.Url = new Uri(url);
+                }));          
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else if(!checkBox1.Checked)
+            {
+                sre.RecognizeAsyncCancel();
+            }
+        }
+    } // Form
+} // ns
