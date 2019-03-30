961ad1ee2f7b2c78028613b85ef34feddbaf0613
David <dop4398@rit.edu>
2019-03-30 01:59:35 -0400

    Got a command mostly working.

---
+++ Listener_Bot/Listener_Bot/Core/Commands/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace Listener_Bot.Core.Commands
+{
+    class Hello : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello there")]
+        [Alias("Hello there!")]
+        [Summary("Hello command")]
+
+        public async Task HelloThere()
+        {
+            await Context.Channel.SendMessageAsync("General Kenobi! You are a bold one...");
+        }
+    }
+}
--- Listener_Bot/Listener_Bot/Program.cs
+++ Listener_Bot/Listener_Bot/Program.cs
@@ -8,12 +8,14 @@ using Discord;
 using Discord.WebSocket;
 using Discord.Commands;
 
+// Where I left off: https://www.youtube.com/watch?v=vUPWrRQsEeE
+
 namespace Listener_Bot
 {
     class Program
     {
-        private DiscordSocketClient Client;
-        private CommandService Commands;
+        private DiscordSocketClient _client;
+        private CommandService _commands;
 
         static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
@@ -22,13 +24,13 @@ namespace Listener_Bot
         private async Task MainAsync()
         {
             // Configure the client
-            Client = new DiscordSocketClient(new DiscordSocketConfig
+            _client = new DiscordSocketClient(new DiscordSocketConfig
             {
                 LogLevel = LogSeverity.Debug
             });
 
             // Configure the command service
-            Commands = new CommandService(new CommandServiceConfig
+            _commands = new CommandService(new CommandServiceConfig
             {
                 CaseSensitiveCommands = true,
                 DefaultRunMode = RunMode.Async,
@@ -36,31 +38,52 @@ namespace Listener_Bot
             });
 
             // Configure the methods
-            Client.MessageReceived += Client_MessageReceived;
-            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            _client.MessageReceived += Client_MessageReceived;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), services: null);
 
-            Client.Ready += Client_Ready;
-            Client.Log += Client_Log;
+            _client.Ready += Client_Ready;
+            _client.Log += Client_Log;
 
             string token = "NTYxMzAzNjMyODQ3NTY4OTM3.XJ6RcQ.LGys9F0l9itKnl4LuvgSO8e4Jcc";
-            await Client.LoginAsync(TokenType.Bot, token);
-            await Client.StartAsync();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
             await Task.Delay(-1);
         }
+
 
         private async Task Client_Log(LogMessage message)
         {
             Console.WriteLine($"[{DateTime.Now} at {message.Source}] {message.Message}");
         }
 
+
         private async Task Client_Ready()
         {
-            await Client.SetGameAsync("Listening");
+            await _client.SetGameAsync("Listening");
         }
 
-        private async Task Client_MessageReceived(SocketMessage arg)
+
+        private async Task Client_MessageReceived(SocketMessage messageParam)
         {
-            // Configure the commands here
+            var message = messageParam as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+
+            if(context.Message == null || context.Message.Content == "" || context.User.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+            if (!(message.HasStringPrefix("!a ", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)))
+            {
+                return;
+            }
+
+            var result = await _commands.ExecuteAsync(context, argPos, services: null);
+            if(!result.IsSuccess)
+            {
+                Console.WriteLine($"[{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {context.Message.Content} | Error: {result.ErrorReason}");
+            }
         }
     }
 }
